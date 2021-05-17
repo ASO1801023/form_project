a770db58c484e517e9962d013cc86665f70c8539
ASO1801023 <1801023@s.asojuku.ac.jp>
2021-05-17 23:09:11 +0900

    データベース消す

--- form_project/settings.py
+++ form_project/settings.py
@@ -74,13 +74,6 @@ WSGI_APPLICATION = 'form_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
