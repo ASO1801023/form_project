3e9dac4ad45cde2a6022a69008ff6dd56a9d8e67
ASO1801023 <1801023@s.asojuku.ac.jp>
2021-05-17 23:18:14 +0900

    データベース消す

--- form_project/settings.py
+++ form_project/settings.py
@@ -75,6 +75,8 @@ WSGI_APPLICATION = 'form_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
