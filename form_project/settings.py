4f77631e3b77f3f641e7d8a39699d87d91edcc1c
ASO1801023 <1801023@s.asojuku.ac.jp>
2021-05-17 22:49:45 +0900

    わかんない

--- form_project/settings.py
+++ form_project/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'form_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': BASE_DIR / 'postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
