6f1e2ac7b1e18bf5d7d8bd08a3a4e81ec0692c13
ASO1801023 <1801023@s.asojuku.ac.jp>
2021-05-18 10:25:20 +0900

    ファイル名変更v２

--- tegoshiritori/urls.py
+++ tegoshiritori/urls.py
@@ -1,4 +1,4 @@
-"""form_project URL Configuration
+"""tegoshiritori URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
