79ee96d67264ffe9f9c7148aa9ac6ed4128e0e48
ASO1801023 <57469549+ASO1801023@users.noreply.github.com>
2021-06-21 15:32:01 +0900

    Update settings.py

--- form_project/settings.py
+++ form_project/settings.py
@@ -18,7 +18,7 @@ pymysql.install_as_MySQLdb()
 # MySQL作戦(1/2) end
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))# Static files (CSS, JavaScript, Images)# https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 
 # Quick-start development settings - unsuitable for production
@@ -134,8 +134,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT =os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/' # localでcssを読み込むためのパス(たぶん)# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'), # サーバでcssを読み込むためのパス
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
