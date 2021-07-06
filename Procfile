08acc99cecccdaf4d0326906327c9deca1aca3c8
email <1801030@s.asojuku.ac.jp>
2021-07-06 17:13:55 +0900

    miuracommit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn form_project.wsgi
---
+++ user/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+ 
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('new/', views.new, name='new'),
+    path('hikaruPath/', views.hikaruSys, name='hikarusSystem'),
+    path('search/', views.search, name='search'),
+    path('list/', views.list, name='list'),
+    path('willCompleteQ/', views.willComplete, name='willComplete'),
+    path('complete/', views.completeSys, name='complete'),
+    path('list_2/', views.list_2, name="list_2"),
+    path('completed/', views.completed, name="completed"),
+    path('random/', views.random, name="completed"),
+]
