c50ea42e3e82ce9dd4b3b78e592fd684aacf88d4
hikal <1801007@s.asojuku.ac.jp>
2021-07-09 11:45:29 +0900

    事件

---
+++ user/models.py
@@ -0,0 +1,88 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.core.mail import send_mail
+from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, UserManager, PermissionsMixin
+
+# Create your models here.
+
+# 仮想Userテーブル
+class UserManager(UserManager):
+    def _create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(_('email address'), unique=True)
+
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = UserManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
+
+class IdeaTree(models.Model):
+    name = models.CharField(max_length=300)
+    overview = models.CharField(max_length=300)
+    complete_flag = models.IntegerField()
+    idea_theme = models.CharField(max_length=300)
+    lastidea_id = models.IntegerField()
+    passcode = models.CharField(max_length=6, default=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Element(models.Model):
+    name = models.CharField(max_length=300)
+    path = models.IntegerField()
+    color = models.IntegerField()
+    ideatree = models.ForeignKey(IdeaTree, on_delete=models.CASCADE)
+    # color 0:textbox 1:類義語 2:しりとり
